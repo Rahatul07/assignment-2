f441d920350d52f7f6bfd220744fde48081f07ff
Rahatul07 <rifarhad08@gmail.com>
2023-11-23 22:31:43 +0600

    professional setup done and the app is listening

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { userRoutes } from './app/modules/users/users.route';
+const app: Application = express();
+// parser
+app.use(express.json());
+app.use(cors());
+// Application routes
+app.use('/api/v1/users', userRoutes);
+app.get('/', (req: Request, res: Response) => {
+  res.send();
+});
+export default app;
